10305c16707c0345d8d2033103c4dac48ab1816a
versx <versefx@gmail.com>
2019-01-27 15:55:04 -0800

    Remove requirements to create new database tables.\r\nAdd support for geofences.\r\nFix issue with retrieving raids for the day.\r\nAdd bootstrap css.\r\nTable style customization.

---
+++ geofence_service.php
@@ -0,0 +1,88 @@
+<?
+class Geofence {
+	public $name;
+	public $polygon;
+}
+class GeofenceService {
+	private $geofences = [];
+	private $dir = "./geofences/";
+	
+	function __construct() {
+		$this->load_geofences();
+	}
+	
+	function load_geofences() {
+		$files = scandir($this->dir);
+		for ($i = 0; $i < count($files); $i++) {
+			if ($files[$i] === '.' || $files[$i] === '..')
+				continue;
+			
+			$geofence = $this->load_geofence($this->dir . $files[$i]);
+			if ($geofence != null) {
+				array_push($this->geofences, $geofence);
+			}
+		}
+		return $this->geofences;
+	}
+
+	function load_geofence($file) {
+		$lines = file($file);
+		$name = "Unknown";
+		if (count($lines) !== 0 && strpos($lines[0], '[') === 0) {
+			$name = str_replace('[', "", $lines[0]);
+			$name = str_replace(']', "", $name);
+		}
+
+		$geofence = new Geofence();
+		$geofence->name = $name;
+		$geofence->polygon = $this->build_polygon(array_slice($lines, 1));
+		return $geofence;
+	}
+
+	function build_polygon($lines) {
+		$polygon = [];
+		$count = count($lines);
+		for ($i = 0; $i < $count; $i++) {
+			$line = $lines[$i];
+			if (strpos($line, '[') === 0)
+				continue;
+			
+			$parts = explode(',', $line);
+			$lat = $parts[0];
+			$lon = $parts[1];
+			array_push($polygon, [$lat, $lon]);
+		}
+
+		return $polygon;
+	}
+
+	public function get_geofence($lat, $lon) {
+		for ($i = 0; $i < count($this->geofences); $i++) {
+			if ($this->is_in_polygon($this->geofences[$i], $lat, $lon)) {
+				return $this->geofences[$i];
+			}
+		}
+		return null;
+	}
+
+	function is_in_polygon($geofence, $lat_x, $lon_y) {
+		//[0]=x,[1]=y
+		$c = 0;
+		$count = count($geofence->polygon);
+		for ($i = -1, $l = $count, $j = $l - 1; ++$i < $l; $j = $i) {
+			try
+			{
+				if ($geofence->polygon == null)
+					continue;
+				
+				$c = (($geofence->polygon[$i][0] <= $lat_x && $lat_x < $geofence->polygon[$j][0] || ($geofence->polygon[$j][0] <= $lat_x && $lat_x < $geofence->polygon[$i][0])) &&
+				($lon_y < ($geofence->polygon[$j][1] - $lon_y) * ($lat_x - $geofence->polygon[$i][0]) / $geofence->polygon[$j][0] - $geofence->polygon[$i][0]) + $geofence->polygon[$i][1]) &&
+				($c = !$c);
+			} catch (Exception $e) {
+				echo $e->getMessage();
+			}
+		}
+		return $c;
+	}
+}
+?>
--- raid.php
+++ raid.php
@@ -1,15 +1,34 @@
 <?php
-$dbhost = "hostname/ip";
-$dbuser = "rdmuser";
-$dbpass = "password";
-$dbname = "rdmdb";
+include 'pokedex.php';
+include 'movesets.php';
+include 'geofence_service.php';
 
+// RealDeviceMap Database
+$dbhost = "127.0.0.1";        // Dtabase host name or IP address
+$dbPort = 3306;               // Database port. (default: 3306)
+$dbuser = "user";             // Database username.
+$dbpass = "password";         // Database user password.
+$dbname = "rdmdb";            // Database name.
+
+// Table Style
+$table_style = "light";       // light/dark
+$table_header_style = "dark"; // light/dark
+$table_striped = true;        // true/false
+
+// StaticMap Lite/Google Maps Url 
+$staticImage = "http://staticmaplite-url-here/staticmap.php?center=%s,%s&markers=%s,%s,red-pushpin&zoom=14&size=300x175&maptype=mapnik"; //https://github.com/dfacts/staticmaplite
+
+
+$googleMapsLink = "https://maps.google.com/maps?q=%s,%s";
+$appleMapsLink = "https://maps.apple.com/maps?daddr=%s,%s";
+
+$geofence_srvc = new GeofenceService();
 $page = $_SERVER['PHP_SELF'];
 $sec = "60";
 
 // Establish connection to database
 try{
-    $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+    $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname;port=$dbPort", $dbuser, $dbpass);
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
@@ -18,55 +37,77 @@ try{
 // Query Database and Build Raid Billboard
 try 
 {
-    $sql = "SELECT time_format(from_unixtime(raid_battle_timestamp), '%h:%i:%s %p'), 
-           time_format(from_unixtime(raid_end_timestamp),'%h:%i:%s %p'), 
-           raid_level, pokedex.name, quick_movedex.name, charge_movedex.name, gym.name, gym.lat, gym.lon from gym 
-           join pokedex on gym.raid_pokemon_id = pokedex.pokemon_id 
-           join quick_movedex on gym.raid_pokemon_move_1 = quick_movedex.move_id 
-           join charge_movedex on gym.raid_pokemon_move_2 = charge_movedex.move_id 
-           where where ST_CONTAINS(ST_GEOMFROMTEXT('POLYGON((
-           LAT -LONG, 
-           LAT -LONG))') 
-           point(gym.lat, gym.lon)) && 
-           raid_pokemon_id is not null && 
-           gym.name is not null 
-           order by raid_end_timestamp";   
-        $result = $pdo->query($sql);
-        if($result->rowCount() > 0){
-            echo "<table border='1';>";
-                echo "<tr>";
-                    echo "<th>Raid Starts</th>";
-                    echo "<th>Raid Ends</th>";
-                    echo "<th>Raid Level</th>";
-                    echo "<th>Raid Boss</th>";
-                    echo "<th>Quick Move</th>";
-                    echo "<th>Charge Move</th>";
-                    echo "<th>Gym</th>";
-                echo "</tr>";
-            while($row = $result->fetch()){
-                echo "<tr>";
-                    echo "<td>" . $row[0] . "</td>";
-                    echo "<td>" . $row[1] . "</td>";
-                    echo "<td>" . $row[2] . "</td>";
-                    echo "<td>" . $row[3] . "</td>";
-                    echo "<td>" . $row[4] . "</td>";
-                    echo "<td>" . $row[5] . "</td>";
-                    echo '<td> <a href="http://www.google.com/maps?q='.$row['lat'].','.$row['lon'].'"" target="_blank" style="color: #137e80">' . $row[6] . '</a></td>';
-                echo "</tr>";
-            }
-            echo "</table>";
-// Free result set
-        unset($result);
-    } else{
-        echo "No records matching your query were found.";
-    }
+	$sql = "
+SELECT 
+	time_format(from_unixtime(raid_battle_timestamp), '%h:%i:%s %p')
+		AS starts, 
+	time_format(from_unixtime(raid_end_timestamp),'%h:%i:%s %p')
+		AS ends, 
+	lat, 
+	lon,
+	raid_level,
+	raid_pokemon_id, 
+	raid_pokemon_move_1,
+	raid_pokemon_move_2,
+	name
+FROM rdmdb.gym
+WHERE
+	raid_pokemon_id IS NOT NULL && 
+	name IS NOT NULL && 
+	raid_end_timestamp >= UNIX_TIMESTAMP()
+ORDER BY 
+	raid_end_timestamp;
+";
+
+	$result = $pdo->query($sql);
+	if($result->rowCount() > 0){
+		echo "<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css' integrity='sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS' crossorigin='anonymous'>";
+		echo "<table class='table table-".$table_style." ".($table_striped ? 'table-striped' : null)."' border='1';>";
+		echo "<thead class='thead-".$table_header_style."'>";
+			echo "<tr>";
+				echo "<th>Raid Starts</th>";
+				echo "<th>Raid Ends</th>";
+				echo "<th>Raid Level</th>";
+				echo "<th>Raid Boss</th>";
+				echo "<th>Moveset</th>";
+				echo "<th>City</th>";
+				echo "<th>Gym</th>";
+			echo "</tr>";
+		echo "</thead>";
+		while($row = $result->fetch()){	
+			$geofence = $geofence_srvc->get_geofence($row['lat'], $row['lon']);
+			$city = ($geofence == null ? "Unknown" : $geofence->name);
+			$map_link = sprintf($googleMapsLink, $row["lat"], $row["lon"]);
+			$pokemon = $pokedex[$row['raid_pokemon_id']];
+			$fast_move = $quick_moves[$row['raid_pokemon_move_1']];
+			$charge_move = $charge_moves[$row['raid_pokemon_move_2']];
+			echo "<tr>";
+				echo "<td scope='row'>" . $row['starts'] . "</td>";
+				echo "<td>" . $row['ends'] . "</td>";
+				echo "<td>" . $row['raid_level'] . "</td>";
+				echo "<td>" . $pokemon . "</td>";
+				echo "<td>" . $fast_move . "/" . $charge_move . "</td>";
+				echo "<td>" . $city . "</td>";
+				echo "<td><a href='" . $map_link . "' target='_blank'>" . $row['name'] . "</a></td>";
+				//echo "<img src='" . sprintf($staticImage, $row["lat"], $row["lon"], $row["lat"], $row["lon"]) . "' height='128' width='256' /></td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+		echo "<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>";
+		echo "<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js' integrity='sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut' crossorigin='anonymous'></script>";
+		echo "<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js' integrity='sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k' crossorigin='anonymous'></script>";
+		// Free result set
+		unset($result);
+	} else{
+		echo "No records matching your query were found.";
+	}
 } catch(PDOException $e){
     die("ERROR: Could not able to execute $sql. " . $e->getMessage());
 }
 // Close connection
 unset($pdo);
-?>
 
+?>
 <html>
     <head>
     <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
