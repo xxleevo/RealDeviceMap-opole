f1bcb1291a7e73491db9c1f51a842034eb56bca9
versx <versefx@gmail.com>
2019-01-28 17:55:43 -0800

    Auto-refresh table

---
+++ data_fetcher.php
@@ -0,0 +1,107 @@
+<?php
+require_once './vendor/autoload.php';
+require_once './config.php';
+require_once './pokedex.php';
+require_once './movesets.php';
+require_once './geofence_service.php';
+
+$geofence_srvc = new GeofenceService();
+
+// Establish connection to database
+try {
+  $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname;port=$dbPort", $dbuser, $dbpass);
+  // Set the PDO error mode to exception
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+  die("ERROR: Could not connect. " . $e->getMessage());
+}
+// Query Database and Build Raid Billboard
+try {
+  $sql = "
+SELECT 
+    time_format(from_unixtime(raid_battle_timestamp), '%h:%i:%s %p')
+        AS starts, 
+    time_format(from_unixtime(raid_end_timestamp),'%h:%i:%s %p')
+        AS ends, 
+    lat, 
+    lon,
+    raid_level,
+    raid_pokemon_id, 
+    raid_pokemon_move_1,
+    raid_pokemon_move_2,
+    name,
+    team_id,
+    ex_raid_eligible
+FROM 
+    rdmdb.gym
+WHERE
+    raid_pokemon_id IS NOT NULL && 
+    name IS NOT NULL &&
+    raid_end_timestamp >= UNIX_TIMESTAMP()
+ORDER BY 
+    raid_end_timestamp;
+";
+
+  $result = $pdo->query($sql);
+  if($result->rowCount() > 0){
+    echo $filters;
+    echo "<table class='table table-".$table_style." ".($table_striped ? 'table-striped' : null)."' border='1' id='gym-table';>";
+    echo "<thead class='thead-".$table_header_style."'>";
+    echo "<tr>";
+        echo "<th>Raid Starts</th>";
+        echo "<th>Raid Ends</th>";
+        echo "<th>Raid Level</th>";
+        echo "<th>Raid Boss</th>";
+        echo "<th>Moveset</th>";
+        echo "<th>City</th>";
+        echo "<th>Team</th>";
+        echo "<th>Ex-Eligible</th>";
+        echo "<th>Gym</th>";
+    echo "</tr>";
+    echo "</thead>";
+    while($row = $result->fetch()){	
+      $geofence = $geofence_srvc->get_geofence($row['lat'], $row['lon']);
+      $city = ($geofence == null ? $unknown_value : $geofence->name);
+      $map_link = sprintf($googleMapsLink, $row["lat"], $row["lon"]);
+      $pokemon = $pokedex[$row['raid_pokemon_id']];
+      $fast_move = $quick_moves[$row['raid_pokemon_move_1']];
+      $charge_move = $charge_moves[$row['raid_pokemon_move_2']];
+      $moveset = (($fast_move == $unknown_value && $charge_move == $unknown_value) ? $unknown_value : $fast_move . "/" . $charge_move);
+      echo "<tr>";
+        echo "<td scope='row'><a title='Remove' data-toggle='tooltip' class='delete'>X&nbsp;</a>" . $row['starts'] . "</td>";
+        echo "<td>" . $row['ends'] . "</td>";
+        echo "<td>" . $row['raid_level'] . "</td>";
+        echo "<td>" . $pokemon . "</td>";
+        echo "<td>" . $moveset . "</td>";
+        echo "<td>" . $city . "</td>";
+        echo "<td>" . get_team($row['team_id']) . "</td>";
+        echo "<td>" . ($row['ex_raid_eligible'] ? "Yes" : "No") . "</td>";
+        echo "<td><a href='" . $map_link . "' target='_blank'>" . $row['name'] . "</a></td>";
+      echo "</tr>";
+    }
+    echo "</table>";
+		
+  // Free result set
+  unset($result);
+  } else{
+    echo "<p>No raids available.</p>";
+  }
+} catch(PDOException $e){
+  die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+}
+// Close connection
+unset($pdo);
+
+function get_team($team_id) {
+  switch ($team_id) {
+    case "1":
+      return "Mystic";
+    case "2":
+      return "Valor";
+    case "3":
+      return "Instinct";
+    default:
+      return "Neutral";
+  }
+}
+?>
--- index.php
+++ index.php
@@ -11,7 +11,6 @@ $appleMapsLink = "https://maps.apple.com/maps?daddr=%s,%s";
 $geofence_srvc = new GeofenceService();
 
 $filters = "
-<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css' integrity='sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS' crossorigin='anonymous'>
 <div class='panel panel-default'>
 <div class='form-group row'>
   <div class='col-md-4'> 
@@ -79,91 +78,10 @@ $filters = "
 </div>
 ";
 
-// Establish connection to database
-try {
-  $pdo = new PDO("mysql:host=$dbhost;dbname=$dbname;port=$dbPort", $dbuser, $dbpass);
-  // Set the PDO error mode to exception
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e){
-  die("ERROR: Could not connect. " . $e->getMessage());
-}
-// Query Database and Build Raid Billboard
-try {
-  $sql = "
-SELECT 
-    time_format(from_unixtime(raid_battle_timestamp), '%h:%i:%s %p')
-        AS starts, 
-    time_format(from_unixtime(raid_end_timestamp),'%h:%i:%s %p')
-        AS ends, 
-    lat, 
-    lon,
-    raid_level,
-    raid_pokemon_id, 
-    raid_pokemon_move_1,
-    raid_pokemon_move_2,
-    name,
-    team_id,
-    ex_raid_eligible
-FROM 
-    rdmdb.gym
-WHERE
-    raid_pokemon_id IS NOT NULL && 
-    name IS NOT NULL &&
-    raid_end_timestamp >= UNIX_TIMESTAMP()
-ORDER BY 
-    raid_end_timestamp;
-";
+echo "<div id='table-refresh'>";
+include_once("data_fetcher.php");
+echo "</div>";
 
-  $result = $pdo->query($sql);
-  if($result->rowCount() > 0){
-    echo $filters;
-    echo "<table class='table table-".$table_style." ".($table_striped ? 'table-striped' : null)."' border='1' id='gym-table';>";
-    echo "<thead class='thead-".$table_header_style."'>";
-    echo "<tr>";
-        echo "<th>Raid Starts</th>";
-        echo "<th>Raid Ends</th>";
-        echo "<th>Raid Level</th>";
-        echo "<th>Raid Boss</th>";
-        echo "<th>Moveset</th>";
-        echo "<th headers='city'>City</th>";
-        echo "<th>Team</th>";
-        echo "<th>Ex-Eligible</th>";
-        echo "<th>Gym</th>";
-    echo "</tr>";
-    echo "</thead>";
-    while($row = $result->fetch()){	
-      $geofence = $geofence_srvc->get_geofence($row['lat'], $row['lon']);
-      $city = ($geofence == null ? $unknown_value : $geofence->name);
-      $map_link = sprintf($googleMapsLink, $row["lat"], $row["lon"]);
-      $pokemon = $pokedex[$row['raid_pokemon_id']];
-      $fast_move = $quick_moves[$row['raid_pokemon_move_1']];
-      $charge_move = $charge_moves[$row['raid_pokemon_move_2']];
-      $moveset = (($fast_move == $unknown_value && $charge_move == $unknown_value) ? $unknown_value : $fast_move . "/" . $charge_move);
-      echo "<tr>";
-        echo "<td scope='row'><a title='Remove' data-toggle='tooltip' class='delete'>X&nbsp;</a>" . $row['starts'] . "</td>";
-        echo "<td>" . $row['ends'] . "</td>";
-        echo "<td>" . $row['raid_level'] . "</td>";
-        echo "<td>" . $pokemon . "</td>";
-        echo "<td>" . $moveset . "</td>";
-        echo "<td>" . $city . "</td>";
-        echo "<td>" . get_team($row['team_id']) . "</td>";
-        echo "<td>" . ($row['ex_raid_eligible'] ? "Yes" : "No") . "</td>";
-        echo "<td><a href='" . $map_link . "' target='_blank'>" . $row['name'] . "</a></td>";
-      echo "</tr>";
-    }
-    echo "</table>";
-    echo "<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>";
-    echo "<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js' integrity='sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut' crossorigin='anonymous'></script>";
-    echo "<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js' integrity='sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k' crossorigin='anonymous'></script>";
-		
-  // Free result set
-  unset($result);
-  } else{
-    echo "<p>No raids available.</p>";
-  }
-} catch(PDOException $e){
-  die("ERROR: Could not able to execute $sql. " . $e->getMessage());
-}
 // Close connection
 unset($pdo);
 
@@ -189,29 +107,29 @@ if ($google_adsense_id != "") {
   });
 </script>";
 }
-
-function get_team($team_id) {
-  switch ($team_id) {
-    case "1":
-      return "Mystic";
-    case "2":
-      return "Valor";
-    case "3":
-      return "Instinct";
-    default:
-      return "Neutral";
-  }
-}
-
 ?>
 <html>
-<script>
+<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css' integrity='sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS' crossorigin='anonymous'>
+<script type="text/javascript" src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
+<script type="text/javascript" src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js' integrity='sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut' crossorigin='anonymous'></script>
+<script type="text/javascript" src='https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js' integrity='sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k' crossorigin='anonymous'></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+
+<script type="text/javascript">
+var refresher = setInterval(filter_raids, 5000);
+setTimeout(function() {
+  clearInterval(refresher);
+}, 1800000);
+
 $(document).on("click", ".delete", function(){
 	$(this).parents("tr").remove();
 	$(".add-new").removeAttr("disabled");
 });
 
 function filter_raids() {
+  var table = $("table-refresh");
+  table.load("data_fetcher.php");
+	
   var search_filter = document.getElementById("search-input").value.toUpperCase();
   var city_filter = document.getElementById("filter-city").value.toUpperCase();
   var level_filter = document.getElementById("filter-level").value.toUpperCase();
@@ -249,6 +167,8 @@ function filter_raids() {
   for (var i = 0; i < tr.length; i++) {
     if (i == 0)
       continue;
+  
+  console.log("Rows:", table.rows[i]);
 	
     var level_value = table.rows[i].cells[2].innerHTML;
     var pkmn_value = table.rows[i].cells[3].innerHTML.toUpperCase();
