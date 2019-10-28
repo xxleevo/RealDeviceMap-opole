59cf70ab941b1ef267a7911df24aa2f63683e388
root <cubecraft@gmail.com>
2019-10-28 21:41:38 +0100

    fixed 404 softcoded and fall formstrings

--- pages/404.php
+++ pages/404.php
@@ -1,8 +1,17 @@
 <?php 
 echo 
+"<div style='margin-left:10px;'>" .
 "<b>ERROR</b><br>
-Diese Seite ist ausschließlich für Spender unseres Projektes.<br>
-Wenn du uns Supporten möchtest, dann klicke <a href='https://www.patreon.com/rocketmapdo'> hier </a>
-" 
+Diese Seite ist ausschließlich für Spender unseres Projektes.";
+if (!empty($config['urls']['paypal']) && !empty($config['urls']['patreon'])) {
+    echo "<br>Wenn du uns Supporten möchtest, dann findest du uns hier:" .
+        "<br><a href='". $config['urls']['paypal'] ."'> Paypal </a>" .
+        "<br><a href='". $config['urls']['patreon'] ."'> Patreon </a>";
+} else if (!empty($config['urls']['patreon'])) {
+    echo "<br>Wenn du uns Supporten möchtest, dann klicke <a href='". $config['urls']['patreon'] ."'> hier </a>";
+}else if (!empty($config['urls']['paypal'])) {
+    echo "<br>Wenn du uns Supporten möchtest, dann klicke <a href='". $config['urls']['paypal'] ."'> hier </a>";
+}
+echo "</div>";
 ?>
 <link rel="stylesheet" href="./static/css/footerfix.css"/>
